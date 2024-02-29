ae515500d54695b98604f7ff21adf3e5e17b8784
hhmbo <hhmbo@PC>
2024-02-29 10:36:52 +0100

    Add project files.

---
+++ ToUpperCase/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace ToUpperCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your lower char");
+
+            char input = Console.ReadKey().KeyChar;
+
+            if (char.IsLower(input))
+            {
+                char uppercase = char.ToUpper(input);
+                Console.WriteLine($"\n Your upper case letter: {uppercase}");
+            }
+            else
+                Console.WriteLine("\n Invalid char");
+
+
+
+        }
+    }
+}
